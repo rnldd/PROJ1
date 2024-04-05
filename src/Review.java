98a6407480174b704a115c851fa83fe1c64065b6
Ronald <rnldkoning@gmail.com>
2024-04-05 11:06:13 +0200

    review class add

---
+++ src/Review.java
@@ -0,0 +1,8 @@
+class Review {
+    public int reviewID;
+    private double beoordelingGameplay;
+    private double beoordelingGraphics;
+    private double BeoordelingStoryline;
+    private double beoordelingGemiddeld;
+    private String beschrijving;
+}
