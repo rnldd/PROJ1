e3c54b6b33865630803ec92851dbf187aeffa68a
Kessoe <wesselbvandermeer@gmail.com>
2024-05-02 13:51:46 +0200

    Keuzemenu in main deels werkend door 'case'

--- src/Main.java
+++ src/Main.java
@@ -2,45 +2,83 @@ import java.util.Scanner;
 
 public class Main {
 
-    public void keuzeSchermGenres(){
+    public static void keuzeSchermGenres(){
         System.out.println("Kies een genre:");
         System.out.println("1. Adventure");
         System.out.println("2. Strategy");
         System.out.println("3. Shooter");
-        System.out.println("5. Exit");
+        System.out.println("4. Exit");
     }
 
-    public void keuzeSchermAdventure(){
+    public static void keuzeSchermAdventure(){
         System.out.println("Van welke game wil je reviews zien? Maak een keuze:");
         System.out.println("1. The Legend of Zelda");
         System.out.println("2. Castlevania");
         System.out.println("3. Final Fantasy VII");
-        System.out.println("4. Exit");
+        System.out.println("4. Terug");
+        System.out.println("5. Exit");
     }
 
-    public void keuzeSchermStrategy(){
+    public static void keuzeSchermStrategy(){
         System.out.println("Van welke game wil je reviews zien? Maak een keuze:");
         System.out.println("1. Tetris");
         System.out.println("2. Warcraft");
         System.out.println("3. Dune II");
-        System.out.println("4. Exit");
+        System.out.println("4. Terug");
+        System.out.println("5. Exit");
     }
 
-    public void keuzeSchermShooter(){
+    public static void keuzeSchermShooter(){
         System.out.println("Van welke game wil je reviews zien? Maak een keuze:");
         System.out.println("1. Doom");
         System.out.println("2. Call of Duty");
         System.out.println("3. Half-Life");
-        System.out.println("4. Exit");
+        System.out.println("4. Terug");
+        System.out.println("5. Exit");
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int keuze;
 
-        //keuzeSchermGenres();
+        do {
+            keuzeSchermGenres();
+            keuze = scanner.nextInt();
 
-        keuze = scanner.nextInt();
+            switch (keuze) {
+                case 1: // Adventure genre
+                    do {
+                        keuzeSchermAdventure();
+                        keuze = scanner.nextInt();
+                        // Hier zouden acties voor de specifieke keuze voor Adventure games geïmplementeerd worden.
+                    } while (keuze != 4);
+                    break;
+                case 2: // Strategy genre
+                    do {
+                        keuzeSchermStrategy();
+                        keuze = scanner.nextInt();
+                        // Hier zouden acties voor de specifieke keuze voor Strategy games geïmplementeerd worden.
+                    } while (keuze != 4);
+                    break;
+                case 3: // Shooter genre
+                    do {
+                        keuzeSchermShooter();
+                        keuze = scanner.nextInt();
+                        // Hier zouden acties voor de specifieke keuze voor Shooter games geïmplementeerd worden.
+                    } while (keuze != 4);
+                    break;
+                case 4: // Exit the main genres menu
+                    System.out.println("Sluit applicatie");
+                    break;
+                default:
+                    System.out.println("Ongeldige keuze, probeer het opnieuw.");
+                    break;
+            }
+        } while (keuze != 4);
+
+        scanner.close();
+
+        // TODO case voor reviews per game laten zien (daarvoor moeten er eerst reviews bestaan)
 
         // Testnaam
         Klant klant = new Klant("Hannie de Boer");
