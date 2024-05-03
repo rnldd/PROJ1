26a16cfb66d711b2ea21c963777de101aaa116ab
Ronald <rnldkoning@gmail.com>
2024-05-03 10:00:20 +0200

    main update

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,45 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void keuzeSchermGenres(){
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int keuze;
+        final int adventuregenre = 1;
+        final int strategygenre = 2;
+        final int shootergenre = 3;
+        final int exit = 4;
+        boolean startMenu = true;
+
+        do {
+            printStartmenu();
+            keuze = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (keuze) {
+                case adventuregenre -> {
+                    startMenu = false;
+                    keuzeSchermAdventure();
+                    //
+                }
+                case strategygenre -> {
+                    startMenu = false;
+                    keuzeSchermStrategy();
+                    //
+                }
+                case shootergenre -> {
+                    startMenu = false;
+                    keuzeSchermShooter();
+                    //
+                }
+                case exit -> {
+                startMenu = false;
+                System.out.println("Sluit applicatie");
+            }
+                default -> System.out.println("Ongeldige keuze, probeer het opnieuw.");
+            }
+        } while (startMenu);
+    }
+    public static void printStartmenu(){
         System.out.println("Kies een genre:");
         System.out.println("1. Adventure");
         System.out.println("2. Strategy");
@@ -35,69 +73,5 @@ public class Main {
         System.out.println("3. Half-Life");
         System.out.println("4. Terug");
         System.out.println("5. Exit");
-    }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int keuze;
-
-        do {
-            keuzeSchermGenres();
-            keuze = scanner.nextInt();
-
-            switch (keuze) {
-                case 1: // Adventure genre
-                    do {
-                        keuzeSchermAdventure();
-                        keuze = scanner.nextInt();
-                        // Hier zouden acties voor de specifieke keuze voor Adventure games geïmplementeerd worden.
-                    } while (keuze != 4);
-                    break;
-                case 2: // Strategy genre
-                    do {
-                        keuzeSchermStrategy();
-                        keuze = scanner.nextInt();
-                        // Hier zouden acties voor de specifieke keuze voor Strategy games geïmplementeerd worden.
-                    } while (keuze != 4);
-                    break;
-                case 3: // Shooter genre
-                    do {
-                        keuzeSchermShooter();
-                        keuze = scanner.nextInt();
-                        // Hier zouden acties voor de specifieke keuze voor Shooter games geïmplementeerd worden.
-                    } while (keuze != 4);
-                    break;
-                case 4: // Exit the main genres menu
-                    System.out.println("Sluit applicatie");
-                    break;
-                default:
-                    System.out.println("Ongeldige keuze, probeer het opnieuw.");
-                    break;
-            }
-        } while (keuze != 4);
-
-        scanner.close();
-
-        // TODO case voor reviews per game laten zien (daarvoor moeten er eerst reviews bestaan)
-
-        // Testnaam
-        Klant klant = new Klant("Hannie de Boer");
-
-        // Test reviews
-        klant.koppelReview(new Review(12345, 8.5, 9.0, 7.0, "Geweldig spel met prachtige graphics, maar het verhaal is een beetje kort", klant));
-        klant.koppelReview(new Review(67890, 7.5, 6.5, 9.0, "Leuk spel, maar de graphics kunnen beter", klant));
-
-        // Output in console
-        System.out.println("Reviews geschreven door klant: " + klant.getKlantNaam());
-
-        for (Review review : klant.getGeschrevenReviews()) {
-            System.out.println("Review ID: " + review.getReviewID());
-            System.out.println("Gameplay beoordeling: " + review.getBeoordelingGameplay());
-            System.out.println("Graphics beoordeling: " + review.getBeoordelingGraphics());
-            System.out.println("Storyline beoordeling: " + review.getBeoordelingStoryline());
-            System.out.printf("Gemiddelde beoordeling: %.2f \n", review.getGemiddelde()); // 2 decimalen
-            System.out.println("Beschrijving: " + review.getBeschrijving());
-            System.out.println();
-        }
     }
 }
