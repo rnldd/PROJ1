1933e4a0c57eb5833f73fe82606b5079906a7b63
sebas <SebasVerbove2003@gmail.com>
2024-05-09 19:14:17 +0200

    pasprijsaan verbetert

--- src/Beheerder.java
+++ src/Beheerder.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Beheerder extends Gebruiker {
     public Game game;
     public Beheerder(int gebruikersID, Game game) {
@@ -5,6 +7,8 @@ public class Beheerder extends Gebruiker {
         this.game = game;
     }
     public void pasPrijsAan(double nieuwePrijs) {
+        System.out.println("Wat is de nieuwe prijs?");
         game.setPrijsGame(nieuwePrijs);
+        System.out.println("De prijs is nu: €" + nieuwePrijs + ".");
     }
 }
