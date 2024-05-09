f869d932d07e0238d131931d18f59d63a3c1ecfa
rnldd <rnldkoning@gmail.com>
2024-05-09 18:07:31 +0200

    main en menu

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         UserManager userManager = new UserManager();
         Security security = new Security(userManager);
-        Menu menu = new Menu(security);
+        security.login();
+        Menu menu = new Menu();
     }
 }
--- src/Menu.java
+++ src/Menu.java
@@ -1,16 +1,6 @@
 import java.util.Scanner;
 
 public class Menu {
-
-    Security security;
-
-    Menu(Security security){
-        this.security = security;
-    }
     Scanner scanner = new Scanner(System.in);
-
-    public void startMenu(Security security){
-        security.login();
-    }
 
 }
