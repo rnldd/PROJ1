2023e5682d97b31d6b01411cadf1dac2802eee0c
rnldd <rnldkoning@gmail.com>
2024-05-09 18:04:46 +0200

    korting update

--- src/Korting.java
+++ src/Korting.java
@@ -1,4 +1,10 @@
 public class Korting {
+
+    private int percentage;
+
+    public Korting(int percentage){
+        this.percentage = percentage;
+    }
     public static int berekenNieuwePrijs(Game game, int percentage) {
         return (int)(game.getPrijsGame() * (1 - percentage / 100.0));
     }
