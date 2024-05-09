deb306b5c3fe9931037806cd2fbac1b8a60dc418
Kessoe <wesselbvandermeer@gmail.com>
2024-05-09 19:19:33 +0200

    Klant aangepast

--- src/Game.java
+++ src/Game.java
@@ -37,4 +37,8 @@ public class Game {
         }
         return somGemiddelden / aantal;
     }
+
+    public String getTitelGame() {
+        return titelGame;
+    }
 }
--- src/Klant.java
+++ src/Klant.java
@@ -1,15 +1,31 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
-public class Klant {
+public class Klant extends Gebruiker{
     private ArrayList<Review> geschrevenReviews;
 
-    public Klant(String klantNaam) {
+    public Klant(String naam) {
+        super(naam);
         this.geschrevenReviews = new ArrayList<>();
     }
-    public void schrijfReview(){
+    public void schrijfReview(Game game, Scanner scanner){
+        System.out.println("Schrijf je review voor: " + game.getTitelGame());
+        System.out.print("Gameplay beoordeling (1-10): ");
+        double gameplayBeoordeling = scanner.nextDouble();
+        System.out.print("Graphics beoordeling (1-10): ");
+        double graphicsBeoordeling = scanner.nextDouble();
+        System.out.print("Storyline beoordeling (1-10): ");
+        double storylineBeoordeling = scanner.nextDouble();
+        scanner.nextLine();
+        System.out.print("Beschrijving van de review: ");
+        String beschrijving = scanner.nextLine();
 
+        Review review = new Review(geschrevenReviews.size() + 1, gameplayBeoordeling, graphicsBeoordeling, storylineBeoordeling, beschrijving, this, game);
+        geschrevenReviews.add(review);
+        game.addReview(review);
     }
     public void toonReviews(){
+        // TODO deze methode
     }
 }
 
