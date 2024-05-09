28acd1b6155cd008b4e30e5973bd722f13f7d6b3
rnldd <rnldkoning@gmail.com>
2024-05-09 18:12:31 +0200

    startmenu

--- src/Menu.java
+++ src/Menu.java
@@ -3,4 +3,15 @@ import java.util.Scanner;
 public class Menu {
     Scanner scanner = new Scanner(System.in);
 
+    public void startMenu() {
+        boolean startMenu = true;
+        while (startMenu) {
+            printStartmenu();
+        }
+    }
+
+    private void printStartmenu() {
+        System.out.println("1");
+        System.out.println("2");
+    }
 }
